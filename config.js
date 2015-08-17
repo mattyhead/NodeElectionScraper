4ed91afe7d53f055a4763e23e721886c9d54bf6c
Matthew Murphy <mattyhead@gmail.com>
2015-08-17 10:01:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.instances=6,  \\ number nightmare instances allowed
-config.uname="",      \\ ssh login -- we'll test for this to decide whether to push over ssh
+config.instances=6, // number nightmare instances allowed
+config.uname="",    // ssh login -- we'll test for this to decide whether to push over ssh
 config.password="",
 config.server="",
 config.path="";
